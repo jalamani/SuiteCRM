fddfd6f14935abfdd9e9cf431dd83973ebc7d2b9
jalamani <jpdalamani@gmail.com>
2019-10-21 12:56:06 -0700

    Fixed alt-Country field in Client module to not be required

--- custom/Extension/modules/Contacts/Ext/Vardefs/_override_sugarfield_alt_address_country.php
+++ custom/Extension/modules/Contacts/Ext/Vardefs/_override_sugarfield_alt_address_country.php
@@ -1,8 +1,5 @@
 <?php
- // created: 2019-10-18 00:33:14
-$dictionary['Contact']['fields']['alt_address_country']['required']=true;
-$dictionary['Contact']['fields']['alt_address_country']['inline_edit']=true;
-$dictionary['Contact']['fields']['alt_address_country']['comments']='Country for alternate address';
-$dictionary['Contact']['fields']['alt_address_country']['merge_filter']='disabled';
+ // created: 2019-10-21 21:55:12
+$dictionary['Contact']['fields']['alt_address_country']['required']=false;
 
  ?>
