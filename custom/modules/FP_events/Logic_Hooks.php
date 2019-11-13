fa762a8e179c52d422401123c4f39c53140fec10
jalamani <jpdalamani@gmail.com>
2019-11-13 15:18:54 -0800

    Added logic hook to Redirect Timesheet Editview to Calendar

---
+++ custom/modules/FP_events/Logic_Hooks.php
@@ -0,0 +1,11 @@
+<?php
+// Do not store anything in this file that is not part of the array or the hook version.  This file will	
+// be automatically rebuilt in the future. 
+ $hook_version = 1; 
+$hook_array = Array(); 
+// position, file, function 
+$hook_array['after_save'] = Array(); 
+$hook_array['after_save'][] = Array(1, 'Post save redirect', 'custom/modules/FP_events/postsaveRedirect.php','postsaveRedirect', 'redirect'); 
+
+
+?>
