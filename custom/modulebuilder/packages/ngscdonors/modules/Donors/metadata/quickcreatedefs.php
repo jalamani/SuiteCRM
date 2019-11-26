6ead5bd01ae580b9066b8c34dcde4859fabd9f68
jalamani <jpdalamani@gmail.com>
2019-11-26 13:04:04 -0800

    Copied Donor module Edit view for Quick Create view

--- custom/modulebuilder/packages/ngscdonors/modules/Donors/metadata/quickcreatedefs.php
+++ custom/modulebuilder/packages/ngscdonors/modules/Donors/metadata/quickcreatedefs.php
@@ -1,74 +1,184 @@
 <?php
-/**
- *
- * SugarCRM Community Edition is a customer relationship management program developed by
- * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- *
- * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License version 3 as published by the
- * Free Software Foundation with the addition of the following permission added
- * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
- * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
- * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Affero General Public License along with
- * this program; if not, see http://www.gnu.org/licenses or write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA.
- *
- * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
- * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- *
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU Affero General Public License version 3.
- *
- * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "Powered by
- * SugarCRM" logo and "Supercharged by SuiteCRM" logo. If the display of the logos is not
- * reasonably feasible for technical reasons, the Appropriate Legal Notices must
- * display the words "Powered by SugarCRM" and "Supercharged by SuiteCRM".
- */
-
-if (!defined('sugarEntry') || !sugarEntry) {
-    die('Not A Valid Entry Point');
-}
-
 $module_name = 'donor_Donors';
 $_object_name = 'donor_donors';
-$viewdefs[$module_name]['QuickCreate'] = array(
-    'templateMeta' => array(
-        'form' => array('buttons' => array('SAVE', 'CANCEL')),
-        'maxColumns' => '2',
-        'widths' => array(
-            array('label' => '10', 'field' => '30'),
-            array('label' => '10', 'field' => '30'),
+$viewdefs [$module_name] = 
+array (
+  'QuickCreate' => 
+  array (
+    'templateMeta' => 
+    array (
+      'form' => 
+      array (
+        'buttons' => 
+        array (
+          0 => 'SAVE',
+          1 => 'CANCEL',
         ),
-        'includes' => array(
-            array('file' => 'modules/Accounts/Account.js'),
+      ),
+      'maxColumns' => '2',
+      'widths' => 
+      array (
+        0 => 
+        array (
+          'label' => '10',
+          'field' => '30',
         ),
+        1 => 
+        array (
+          'label' => '10',
+          'field' => '30',
+        ),
+      ),
+      'includes' => 
+      array (
+        0 => 
+        array (
+          'file' => 'modules/Accounts/Account.js',
+        ),
+      ),
+      'useTabs' => false,
+      'tabDefs' => 
+      array (
+        'LBL_ACCOUNT_INFORMATION' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+        'LBL_ADDRESS_INFORMATION' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+        'LBL_EMAIL_ADDRESSES' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+        'LBL_DESCRIPTION_INFORMATION' => 
+        array (
+          'newTab' => false,
+          'panelDefault' => 'expanded',
+        ),
+      ),
     ),
-
-    'panels' => array(
-        'lbl_account_information' => array(
-            array(array('name' => 'name', 'displayParams' => array('required' => true)), 'assigned_user_name'),
-            array(
-                'website',
+    'panels' => 
+    array (
+      'lbl_account_information' => 
+      array (
+        0 => 
+        array (
+          0 => 
+          array (
+            'name' => 'name',
+            'displayParams' => 
+            array (
+              'required' => true,
             ),
-            array('industry', array('name' => 'phone_office')),
-            array($_object_name . '_type', 'phone_fax'),
-            array('annual_revenue', ''),
+          ),
+          1 => 
+          array (
+            'name' => 'phone_office',
+          ),
         ),
-        'lbl_email_addresses' => array(
-            array('email1')
+        1 => 
+        array (
+          0 => 'website',
+          1 => 'phone_fax',
         ),
-    )
+        2 => 
+        array (
+          0 => 
+          array (
+            'name' => 'ticker_symbol',
+            'comment' => 'The stock trading (ticker) symbol for the company',
+            'label' => 'LBL_TICKER_SYMBOL',
+          ),
+          1 => 
+          array (
+            'name' => 'phone_alternate',
+            'comment' => 'An alternate phone number',
+            'label' => 'LBL_PHONE_ALT',
+          ),
+        ),
+        3 => 
+        array (
+          0 => 
+          array (
+            'name' => 'rating',
+            'comment' => 'An arbitrary rating for this company for use in comparisons with others',
+            'label' => 'LBL_RATING',
+          ),
+          1 => 
+          array (
+            'name' => 'employees',
+            'comment' => 'Number of employees, varchar to accomodate for both number (100) or range (50-100)',
+            'label' => 'LBL_EMPLOYEES',
+          ),
+        ),
+        4 => 
+        array (
+          0 => 
+          array (
+            'name' => 'ownership',
+            'comment' => '',
+            'label' => 'LBL_OWNERSHIP',
+          ),
+          1 => 'industry',
+        ),
+        5 => 
+        array (
+          0 => 'donor_donors_type',
+          1 => 'annual_revenue',
+        ),
+        6 => 
+        array (
+          0 => 
+          array (
+            'name' => 'amountdonated',
+            'label' => 'LBL_AMOUNTDONATED',
+          ),
+          1 => 'assigned_user_name',
+        ),
+      ),
+      'lbl_address_information' => 
+      array (
+        0 => 
+        array (
+          0 => 
+          array (
+            'name' => 'billing_address_street',
+            'comment' => 'The street address used for billing address',
+            'label' => 'LBL_BILLING_ADDRESS_STREET',
+          ),
+          1 => 
+          array (
+            'name' => 'shipping_address_street',
+            'comment' => 'The street address used for for shipping purposes',
+            'label' => 'LBL_SHIPPING_ADDRESS_STREET',
+          ),
+        ),
+      ),
+      'lbl_email_addresses' => 
+      array (
+        0 => 
+        array (
+          0 => 'email1',
+        ),
+      ),
+      'lbl_description_information' => 
+      array (
+        0 => 
+        array (
+          0 => 
+          array (
+            'name' => 'description',
+            'comment' => 'Full text of the note',
+            'label' => 'LBL_DESCRIPTION',
+          ),
+        ),
+      ),
+    ),
+  ),
 );
+;
+?>
